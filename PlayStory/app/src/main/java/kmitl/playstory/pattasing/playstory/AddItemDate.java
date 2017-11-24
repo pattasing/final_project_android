15b639eee9de5d19dd79c2effc37cd99448460f4
pattasing <pattarawadee-s@hotmail.com>
2017-11-25 02:20:50 +0700

    Update pick Time

--- PlayStory/app/src/main/java/kmitl/playstory/pattasing/playstory/AddItemDate.java
+++ PlayStory/app/src/main/java/kmitl/playstory/pattasing/playstory/AddItemDate.java
@@ -1,16 +1,24 @@
 package kmitl.playstory.pattasing.playstory;
 
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
+import android.text.format.DateFormat;
+
+import java.util.Calendar;
 
 public class AddItemDate extends AppCompatActivity {
 
     private TextView textTime;
-    private TextView textPickTime;
+    protected static TextView textPickTime;
     private TextView textMessage;
     private EditText editMessage;
     private Button buttonPickTime;
@@ -54,5 +62,39 @@ public class AddItemDate extends AppCompatActivity {
         textAddTimeStory2.setTypeface(font);
         textSaveTime.setTypeface(font);
         textCancelTime.setTypeface(font);
+    }
+
+    public void buttonTimePick(View view) {
+        TimePickerFragment mTimePicker = new TimePickerFragment();
+        mTimePicker.show(getFragmentManager(), "Select time");
+    }
+
+    public static class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener{
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            final Calendar calendar = Calendar.getInstance();
+            int hour = calendar.get(Calendar.HOUR_OF_DAY);
+            int minute = calendar.get(Calendar.MINUTE);
+            return new TimePickerDialog(getActivity(), this, hour, minute, DateFormat.is24HourFormat(getActivity()));
+        }
+
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            String hourS = String.valueOf(hourOfDay);
+            String minuteS = String.valueOf(minute);
+
+            if(hourS.length() == 1 && minuteS.length() == 1) {
+                textPickTime.setText("0" + String.valueOf(hourOfDay) + " : " + "0" + String.valueOf(minute));
+            }
+            else if(hourS.length() > 1 && minuteS.length() == 1){
+                textPickTime.setText(String.valueOf(hourOfDay) + " : " +" 0" + String.valueOf(minute));
+            }
+            else if(hourS.length() == 1 && minuteS.length() > 1){
+                textPickTime.setText("0" + String.valueOf(hourOfDay) + " : " + String.valueOf(minute));
+            }
+            else{
+                textPickTime.setText(String.valueOf(hourOfDay) + " : " + String.valueOf(minute));
+            }
+        }
     }
 }
