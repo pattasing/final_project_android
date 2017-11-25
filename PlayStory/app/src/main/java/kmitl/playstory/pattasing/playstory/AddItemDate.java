6efcc792015344f654ba084fc265445e8d2a7d78
pattasing <pattarawadee-s@hotmail.com>
2017-11-25 20:56:56 +0700

    Update PlacePicker

--- PlayStory/app/src/main/java/kmitl/playstory/pattasing/playstory/AddItemDate.java
+++ PlayStory/app/src/main/java/kmitl/playstory/pattasing/playstory/AddItemDate.java
@@ -1,9 +1,15 @@
 package kmitl.playstory.pattasing.playstory;
 
+import android.*;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Typeface;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +18,12 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.text.format.DateFormat;
+import android.widget.Toast;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
 import java.util.Calendar;
 
@@ -29,6 +41,9 @@ public class AddItemDate extends AppCompatActivity {
     private TextView textAddTimeStory2;
     private TextView textSaveTime;
     private TextView textCancelTime;
+
+    private final static int MY_PERMISSION_FINE_LOCATION = 101;
+    private final static int PLACE_PICKER_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +77,17 @@ public class AddItemDate extends AppCompatActivity {
         textAddTimeStory2.setTypeface(font);
         textSaveTime.setTypeface(font);
         textCancelTime.setTypeface(font);
+
+        requestPermission();
+
+    }
+
+    private void requestPermission() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_FINE_LOCATION);
+            }
+        }
     }
 
     public void buttonTimePick(View view) {
@@ -69,7 +95,19 @@ public class AddItemDate extends AppCompatActivity {
         mTimePicker.show(getFragmentManager(), "Select time");
     }
 
-    public static class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener{
+    public void buttonPickMap(View view) {
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+        try {
+            Intent intent = builder.build(AddItemDate.this);
+            startActivityForResult(intent, PLACE_PICKER_REQUEST);
+        } catch (GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
             final Calendar calendar = Calendar.getInstance();
@@ -83,18 +121,43 @@ public class AddItemDate extends AppCompatActivity {
             String hourS = String.valueOf(hourOfDay);
             String minuteS = String.valueOf(minute);
 
-            if(hourS.length() == 1 && minuteS.length() == 1) {
+            if (hourS.length() == 1 && minuteS.length() == 1) {
                 textPickTime.setText("0" + String.valueOf(hourOfDay) + " : " + "0" + String.valueOf(minute));
-            }
-            else if(hourS.length() > 1 && minuteS.length() == 1){
-                textPickTime.setText(String.valueOf(hourOfDay) + " : " +" 0" + String.valueOf(minute));
-            }
-            else if(hourS.length() == 1 && minuteS.length() > 1){
+            } else if (hourS.length() > 1 && minuteS.length() == 1) {
+                textPickTime.setText(String.valueOf(hourOfDay) + " : " + " 0" + String.valueOf(minute));
+            } else if (hourS.length() == 1 && minuteS.length() > 1) {
                 textPickTime.setText("0" + String.valueOf(hourOfDay) + " : " + String.valueOf(minute));
-            }
-            else{
+            } else {
                 textPickTime.setText(String.valueOf(hourOfDay) + " : " + String.valueOf(minute));
             }
         }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case MY_PERMISSION_FINE_LOCATION:
+                if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getApplicationContext(), "This app requires location permissions to be granted", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PLACE_PICKER_REQUEST){
+            if (resultCode == RESULT_OK){
+                Place place = PlacePicker.getPlace(AddItemDate.this, data);
+                textPickLocation.setText(place.getName());
+//                placeAddressText.setText(place.getAddress());
+            }
+        }
+    }
+
+
 }
